0ee165f941e2f688b0c5fbf90ce3db28f095f14a
katiekull <kullkatie@gmail.com>
2022-08-14 17:10:14 -0700

    updates

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -5,7 +5,7 @@
 import formatReservationDate from "./format-reservation-date";
 import formatReservationTime from "./format-reservation-date";
 
-const API_BASE_URL = "https://periodic-tables-be.herokuapp.com";
+const API_BASE_URL = "https://git.heroku.com/kull-backend.git";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
